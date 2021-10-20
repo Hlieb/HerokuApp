ee4ea7a1ec8a55bef3095a3ab1010a67fc8a3b77
Hlieb <hliebboiechko@gmail.com>
2021-10-20 19:50:06 +0200

    Add/Remove Test V2.0

--- src/test/java/tests/AddRemoveElements.java
+++ src/test/java/tests/AddRemoveElements.java
@@ -7,32 +7,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
 
 public class AddRemoveElements {
 
-@Test
+    @Test
     public void AddRemoveElementsTest() {
-    System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-    ChromeOptions chromeOptions = new ChromeOptions();
-    chromeOptions.addArguments("--incognito");
-    chromeOptions.addArguments("--disable-popup-blocking");
-    WebDriver driver = new ChromeDriver(chromeOptions);
-    driver.manage().window().fullscreen();
-    driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
-    WebElement clickAddButton = driver.findElement(By.xpath("//*[@onclick='addElement()']"));
-    clickAddButton.click();
-    clickAddButton.click();
-    WebElement deleteButton = driver.findElement(By.xpath("//*[@class='added-manually']"));
-    deleteButton.click();
-    int addElements = driver.findElements(By.xpath("//*[@onclick='addElement()']")).size();
-    System.out.println("'Add' elements: " + addElements);
-    int deleteElements = driver.findElements(By.xpath("//*[@class='added-manually']")).size();
-    System.out.println("'Delete' elements: " + deleteElements);
-    int allElements = addElements + deleteElements;
-    System.out.println("All elements: " + allElements);
-
-
-}
-
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().fullscreen();
+        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement clickAddButton = driver.findElement(By.xpath("//*[@onclick='addElement()']"));
+        clickAddButton.click();
+        clickAddButton.click();
+        WebElement deleteButton = driver.findElement(By.xpath("//*[@class='added-manually']"));
+        deleteButton.click();
+        int addElements = driver.findElements(By.xpath("//*[@onclick='addElement()']")).size();
+        System.out.println("'Add' elements: " + addElements);
+        int deleteElements = driver.findElements(By.xpath("//*[@class='added-manually']")).size();
+        System.out.println("'Delete' elements: " + deleteElements);
+        int allElements = addElements + deleteElements;
+        System.out.println("All elements: " + allElements);
+        driver.quit();
+    }
 }
